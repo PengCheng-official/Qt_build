15852830fbc45ff0103fd0de26edb34575e9b6f0
StarRainer <2196632542@qq.com>
2024-08-26 19:44:38 +0800

    VERSION 1.0.0

---
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_learn_qt_FILE C:/Users/21966/Desktop/Qt_build/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/learn_qt.exe)
+set(__QT_DEPLOY_TARGET_learn_qt_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_learn_qt_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
